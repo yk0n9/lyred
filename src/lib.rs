8d73ba217cc8b5d9b669b39da6b65c972e69d37a
ykong1337 <ykonghao@icloud.com>
2023-07-25 09:25:21 +0800

    rm test

--- src/lib.rs
+++ src/lib.rs
@@ -3,17 +3,3 @@ pub mod font;
 pub mod maps;
 pub mod midi;
 pub mod ui;
-
-#[cfg(test)]
-mod tests {
-    use crate::midi::Midi;
-    use crate::ui::Mode;
-
-    #[test]
-    fn test() {
-        let mid = Midi::new();
-        mid.init();
-
-        mid.playback(false, Mode::GenShin);
-    }
-}
